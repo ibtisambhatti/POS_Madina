8a660ed55f85661b34e6cc17dc869e12c6cda370
ibtisambhatti <walimuhammad0238@gmail.com>
2024-08-25 20:32:50 +0500

    Renamed Form1.cs to Main ------------------------------ Date 25-August-2024

--- POS_Madina/Main.cs
+++ POS_Madina/Main.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace POS_Madina
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
