2cca17c368644bf75b6271c0fde3e10f11f75f0f
Ahmed Awan <ahmii.awan00@gmail.com>
2024-08-25 12:05:53 -0700

    Updated Program File

--- POS_Madina/Program.cs
+++ POS_Madina/Program.cs
@@ -16,7 +16,7 @@ namespace POS_Madina
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new login());
+            Application.Run(new Signup());
         }
     }
 }
