0745d7c0dc33a76f01427b67c227f70ae7c5e239
TalhaFarooq82 <talha.farooq6625@gmail.com>
2024-08-27 01:03:51 +0500

    "ToastForm.cs" and Notification form i.e "Form1.cs" are added further more Resourses Icons are also added Link of the video: https://youtu.be/vLWWShU9gKY?si=mJuFCM_XDtos5-Ug --------------------------------------------------- Date: 8-August-2024

---
+++ POS_Madina/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS_Madina
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ShowToast("INFO", "This is an info toast");
+        }
+
+        public void ShowToast (string type, string message)
+        {
+            ToastForm toast = new ToastForm(type, message);
+            toast.Show();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ShowToast("SUCCESS", "This is a success toast");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ShowToast("ERROR", "This is an error toast");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ShowToast("WARNING", "This is a warning toast");
+        }
+    }
+}
---
+++ POS_Madina/Toastform.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS_Madina
+{
+    public partial class ToastForm : Form
+    {
+        int toastX, toastY;
+        public ToastForm(string type, string message)
+        {
+            InitializeComponent();
+            IbType.Text = type;
+            IbMessage.Text = message;
+            switch (type)
+            {
+                case "SUCCESS":
+
+                    ToastBorder.BackColor = Color.FromArgb(57, 155, 53);
+                    picIcon.Image = Properties.Resources.tick;
+                    break;
+                case "ERROR":
+                    ToastBorder.BackColor = Color.FromArgb(227, 50, 45);
+                    picIcon.Image = Properties.Resources.Error_icon;
+                    break;
+                case "INFO":
+                    ToastBorder.BackColor = Color.FromArgb(18, 136, 191);
+                    picIcon.Image = Properties.Resources.Excalamation_mark;
+
+                    break;
+                case "WARNING":
+                    ToastBorder.BackColor = Color.FromArgb(245, 171, 35);
+                    picIcon.Image = Properties.Resources.false_icon;
+
+                    break;
+
+
+            }
+        }
+
+        private void ToastForm_Load(object sender, EventArgs e)
+        {
+            Position();
+        }
+
+        private void toastTimer_Tick(object sender, EventArgs e)
+        {
+            toastY -= 10;
+            this.Location = new Point(toastX, toastY);
+            if (toastY <= 750)
+            {
+                toastTimer.Stop();
+                toastHide.Start();
+            }
+        }
+        int y = 100;
+        private void toastHide_Tick(object sender, EventArgs e)
+        {
+            y--;
+            if (y <= 0)
+            {
+                toastY += 1;
+                this.Location = new Point(toastX, toastY += 10);
+                if (toastY >= 800)
+                {
+                    toastHide.Stop();
+                    y = 100;
+                    this.Close();
+                }
+            }
+        }
+
+        private void Position()
+        {
+            int ScreenWidth = Screen.PrimaryScreen.WorkingArea.Width;
+            int ScreenHeight = Screen.PrimaryScreen.WorkingArea.Height;
+
+            toastX = ScreenWidth - this.Width;
+            toastY = ScreenHeight - this.Height;
+
+            this.Location = new Point(toastX, toastY);
+        }
+    }
+}
