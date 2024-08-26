e2ea3bb2703e0889618a7333e39a5e2fc9f39f96
ibtisambhatti <walimuhammad0238@gmail.com>
2024-08-27 03:29:07 +0500

    Added a Category Setup and Grid View --------------- 27-August-2024

---
+++ POS_Madina/SetupCategory.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace POS_Madina
+{
+    partial class SetupCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboCategory = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.labelBrand = new System.Windows.Forms.Label();
+            this.labelCategoryDesc = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelCategory = new System.Windows.Forms.Label();
+            this.txtPdesc = new System.Windows.Forms.TextBox();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            this.textCategory = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.labelCategoryName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cboCategory
+            // 
+            this.cboCategory.FormattingEnabled = true;
+            this.cboCategory.Location = new System.Drawing.Point(174, 180);
+            this.cboCategory.Name = "cboCategory";
+            this.cboCategory.Size = new System.Drawing.Size(531, 24);
+            this.cboCategory.TabIndex = 32;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(347, 280);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 35);
+            this.btnSave.TabIndex = 21;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.BackColor = System.Drawing.Color.Red;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.ForeColor = System.Drawing.Color.Black;
+            this.btnCancel.Location = new System.Drawing.Point(611, 280);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(94, 35);
+            this.btnCancel.TabIndex = 30;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // labelBrand
+            // 
+            this.labelBrand.AutoSize = true;
+            this.labelBrand.Location = new System.Drawing.Point(32, 184);
+            this.labelBrand.Name = "labelBrand";
+            this.labelBrand.Size = new System.Drawing.Size(49, 16);
+            this.labelBrand.TabIndex = 22;
+            this.labelBrand.Text = "Brand :";
+            // 
+            // labelCategoryDesc
+            // 
+            this.labelCategoryDesc.AutoSize = true;
+            this.labelCategoryDesc.Location = new System.Drawing.Point(32, 138);
+            this.labelCategoryDesc.Name = "labelCategoryDesc";
+            this.labelCategoryDesc.Size = new System.Drawing.Size(139, 16);
+            this.labelCategoryDesc.TabIndex = 25;
+            this.labelCategoryDesc.Text = "Category Description :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(416, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 16);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Barcode :";
+            // 
+            // labelCategory
+            // 
+            this.labelCategory.AutoSize = true;
+            this.labelCategory.Location = new System.Drawing.Point(32, 49);
+            this.labelCategory.Name = "labelCategory";
+            this.labelCategory.Size = new System.Drawing.Size(139, 16);
+            this.labelCategory.TabIndex = 23;
+            this.labelCategory.Text = "Category Type Code :";
+            // 
+            // txtPdesc
+            // 
+            this.txtPdesc.Location = new System.Drawing.Point(174, 90);
+            this.txtPdesc.Name = "txtPdesc";
+            this.txtPdesc.Size = new System.Drawing.Size(531, 22);
+            this.txtPdesc.TabIndex = 20;
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Location = new System.Drawing.Point(521, 46);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(184, 22);
+            this.txtBarcode.TabIndex = 19;
+            // 
+            // textCategory
+            // 
+            this.textCategory.Location = new System.Drawing.Point(177, 46);
+            this.textCategory.Name = "textCategory";
+            this.textCategory.Size = new System.Drawing.Size(194, 22);
+            this.textCategory.TabIndex = 18;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnUpdate.FlatAppearance.BorderSize = 0;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.ForeColor = System.Drawing.Color.White;
+            this.btnUpdate.Location = new System.Drawing.Point(479, 280);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(94, 35);
+            this.btnUpdate.TabIndex = 29;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // labelCategoryName
+            // 
+            this.labelCategoryName.AutoSize = true;
+            this.labelCategoryName.Location = new System.Drawing.Point(32, 93);
+            this.labelCategoryName.Name = "labelCategoryName";
+            this.labelCategoryName.Size = new System.Drawing.Size(108, 16);
+            this.labelCategoryName.TabIndex = 26;
+            this.labelCategoryName.Text = "Category Name :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(531, 22);
+            this.textBox1.TabIndex = 35;
+            // 
+            // SetupCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 383);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cboCategory);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.labelBrand);
+            this.Controls.Add(this.labelCategoryDesc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelCategory);
+            this.Controls.Add(this.txtPdesc);
+            this.Controls.Add(this.txtBarcode);
+            this.Controls.Add(this.textCategory);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.labelCategoryName);
+            this.Name = "SetupCategory";
+            this.Text = "SetupCategory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.ComboBox cboCategory;
+        public System.Windows.Forms.Button btnSave;
+        public System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label labelBrand;
+        private System.Windows.Forms.Label labelCategoryDesc;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox txtPdesc;
+        public System.Windows.Forms.TextBox txtBarcode;
+        public System.Windows.Forms.TextBox textCategory;
+        public System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label labelCategoryName;
+        private System.Windows.Forms.Label labelCategory;
+        public System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ POS_Madina/SetupCategory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS_Madina
+{
+    public partial class SetupCategory : Form
+    {
+        public SetupCategory()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
